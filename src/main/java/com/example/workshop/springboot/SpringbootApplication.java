e0f278c8f4bce9a1cb382d7381a5a942cb8858e0
Norberto Enomoto <norberto.enomoto@gmail.com>
2021-12-30 19:36:52 -0300

    Alteracao para suportar a ultima versao mysql

--- src/main/java/com/example/workshop/springboot/SpringbootApplication.java
+++ src/main/java/com/example/workshop/springboot/SpringbootApplication.java
@@ -1,11 +1,22 @@
 package com.example.workshop.springboot;
 
+import java.util.Date;
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootApplication {
 
+    @PostConstruct
+    public void init(){
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));   // It will set UTC timezone
+        System.out.println("Spring boot application running America/Sao_Paulo timezone : " + new Date());   // It will print UTC timezone
+    }
+		
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
 	}
