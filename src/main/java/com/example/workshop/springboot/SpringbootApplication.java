256459216e848e208a74d1a248fa310ecf2ce1ec
Norberto Enomoto <norberto.enomoto@gmail.com>
2022-01-18 17:42:47 -0300

    Alteracao log

--- src/main/java/com/example/workshop/springboot/SpringbootApplication.java
+++ src/main/java/com/example/workshop/springboot/SpringbootApplication.java
@@ -8,6 +8,8 @@ import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
 @Slf4j
 public class SpringbootApplication {
