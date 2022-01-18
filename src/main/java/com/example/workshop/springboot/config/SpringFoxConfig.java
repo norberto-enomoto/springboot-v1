f0ba6b59f67cc498da3eab332478e7f02cbf1b50
Norberto Enomoto <norberto.enomoto@gmail.com>
2022-01-18 17:58:18 -0300

    Alteracoes

--- src/main/java/com/example/workshop/springboot/config/SpringFoxConfig.java
+++ src/main/java/com/example/workshop/springboot/config/SpringFoxConfig.java
@@ -1,4 +1,4 @@
-package com.example.worshop.springboot.config;
+package com.example.workshop.springboot.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
